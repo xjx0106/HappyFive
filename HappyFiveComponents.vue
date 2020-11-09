4f841932d097cbcc6e4e5e0ddcfe82d5e2c2b2ab
xjx0106 <xjx0106@126.com>
2020-11-09 11:59:26 +0800

    将其余控件集成到一个文件里

---
+++ HappyFiveComponents.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- 放在主页面某处。选手选择器 -->
+    <div>
+      <el-select
+        style="width: 80px;margin-left:8px;margin-right:8px"
+        id="happyFiveUser"
+        v-model="happyFiveUser"
+        placeholder="选手!"
+      >
+        <el-option
+          v-for="item in [
+            { value: 'black', label: '黑色' },
+            { value: 'white', label: '白色' }
+          ]"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+    </div>
+    <!-- 放在主页面某处。运行器，打开则开始保持连接和运行 -->
+    <div>
+      <el-switch
+        v-model="constantHappyQuery"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+      >
+      </el-switch>
+    </div>
+    <!-- 放在主页面某处。清除happyFive盘，重新开始按钮 -->
+    <div><span @click="startHappyFive">连接词</span></div>
+    <!-- 放在主页面某处。显示happyFive -->
+    <span @click="showHappyFive">询问词</span>
+    <!-- 放在主页面某处。显示是否轮到自己的红点绿点 -->
+    <div>
+      <span v-if="!nowYourTurnHappyFive" class="status-text" status-red
+        >关系链</span
+      ><span v-if="nowYourTurnHappyFive" class="status-text" status-green
+        >关系链</span
+      >
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {}
+};
+</script>
