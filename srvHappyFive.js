4a337eff790a85c3b679d3da9948f97c98b632f6
xjx0106 <xjx0106@126.com>
2020-11-09 12:01:23 +0800

    第二个版本的服务端代码

--- srvHappyFive.js
+++ srvHappyFive.js
@@ -1,32 +1,48 @@
 var http = require("http"); //获取http请求（前面的文章有详细注释）
-var arr = []; //新建消息容器，存放消息集
+// var arr = []; //新建消息容器，存放消息集
+var happyFiveWhiteBall = []; //新建消息容器，存放消息集
+var happyFiveBlackBall = []; //新建消息容器，存放消息集
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer((req, res) => {
+  // 接收请求参数
   var data = "";
-  req.on("data", function(d) {
+  req.on("data", (d) => {
     data += d;
   });
-  req.on("end", function() {
+
+  // 解析请求参数
+  req.on("end", () => {
     var obj = JSON.parse(data);
-    //解析对象，将user和msg取出
-    // if (obj.id === 1) {
-    arr.push(obj.x + "," + obj.y);
-    // }
+    if (obj.type === "play") {
+      // 添加棋子
+      if (obj.user == "black") {
+        happyFiveBlackBall.push([obj.x, obj.y]);
+      } else if (obj.user == "white") {
+        happyFiveWhiteBall.push([obj.x, obj.y]);
+      }
+    } else if (obj.type === "delete") {
+      // 置空棋盘
+      happyFiveBlackBall = [];
+      happyFiveWhiteBall = [];
+    } else if (obj.type === "query") {
+    }
 
     // 返回数组和错误信息（没有则为空）
     var result = {
-      resu: arr,
-      error: null
+      resu: { happyFiveBlackBall, happyFiveWhiteBall },
+      error: null,
     };
+
+    // 写状态
     res.writeHead(200, {
       "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
     });
     res.write(JSON.stringify(result)); //将打包好的对象传到前端
     res.end();
   });
 });
 // 监听服务
-server.listen(8080, "10.20.20.74", function() {
+server.listen(8080, "10.20.20.74", () => {
   console.log("侦听开始");
 });
